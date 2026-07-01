07223e0183f8160bb41d81dbdf570aced5e87558
SIVASAI <shivasai1872005115@gmail.com>
2026-07-01 17:52:44 +0530

    Commit 25: Add client-side constants file defining file limits and accepted types

---
+++ apps/web/src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = 'AuditMind';
+export const APP_VERSION = '1.0.0';
+export const SUPPORT_EMAIL = 'support@auditmind.app';
+export const FILE_SIZE_LIMIT_MB = 15;
+export const FILE_SIZE_LIMIT_BYTES = FILE_SIZE_LIMIT_MB * 1024 * 1024;
+export const ACCEPTED_EXTENSIONS = ['.pdf', '.docx', '.doc'];
